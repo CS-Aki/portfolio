49b449b1da5c577e189434aa7fe5490480243422
CS-Aki <aquino.sir.victor.bscs2022@gmail.com>
2026-05-04 20:06:58 +0800

    Fix

--- public/index.php
+++ public/index.php
@@ -38,8 +38,7 @@
                 <div class="relative pl-8 sm:pl-12 text-left">
 
                     <div class="bg-[#0a1a2f] rounded-[10px] border border-[#1a644e] p-5 sm:p-6 w-full">
-
-                        <div class="flex justify-between gap-4 mb-4 w-full">
+                        <div class="flex flex-row items-start justify-between gap-4 mb-4 w-full">
                             <!-- Left side -->
                             <div class="flex-1 min-w-0">
                                 <h3 class="text-[#40985e] text-lg sm:text-xl font-bold leading-snug mb-1">
@@ -51,12 +50,9 @@
                             </div>
 
                             <!-- Right side -->
-                            <div class="flex-shrink-0 text-right">
-                                <span class="text-xs sm:text-sm text-[#d1cb95] opacity-60 whitespace-nowrap">
-                                    April 2025 – July 2025
-                                </span>
-                            </div>
-
+                            <span class="flex-shrink-0 text-xs sm:text-sm text-[#d1cb95] opacity-60 whitespace-nowrap mt-1">
+                                April 2025 – July 2025
+                            </span>
                         </div>
 
                         <ul class="space-y-2 text-sm sm:text-base list-none">
