477e266afe96ad79871216e3ddbc7424fb1e1595
CS-Aki <aquino.sir.victor.bscs2022@gmail.com>
2026-05-04 19:37:47 +0800

    Updated

--- public/index.php
+++ public/index.php
@@ -111,10 +111,8 @@
                 </div>
             </div>
 
-            <div class="max-w-[800px] mx-auto mt-8">
-                <!-- Internship -->
+            <!-- <div class="max-w-[800px] mx-auto mt-8">
                 <div class="relative pl-8 sm:pl-12 text-left">
-                    <!-- <div class="absolute -left-[9px] top-1 w-4 h-4 rounded-full bg-[#40985e] border-2 border-[#04373b]"></div> -->
                     <div class="bg-[#0a1a2f] rounded-[10px] border border-[#1a644e] p-5 sm:p-6">
                         <h3 class="text-[#40985e] text-lg sm:text-xl font-bold leading-snug mb-1">
                             Full Stack Laravel Developer Intern
@@ -149,7 +147,7 @@
                         </div>
                     </div>
                 </div>
-            </div>
+            </div> -->
         </div>
     </section>
 
