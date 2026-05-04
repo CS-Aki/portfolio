da3e23877680762af1d1c1190b87dccbf366eab7
CS-Aki <aquino.sir.victor.bscs2022@gmail.com>
2026-05-04 20:02:01 +0800

    test

--- public/index.php
+++ public/index.php
@@ -51,7 +51,7 @@
                             </div>
 
                             <!-- Right side -->
-                            <div class="flex-shrink-0 text-right ml-20">
+                            <div class="flex-shrink-0 text-right ml-40">
                                 <span class="text-xs sm:text-sm text-[#d1cb95] opacity-60 whitespace-nowrap">
                                     April 2025 – July 2025
                                 </span>
