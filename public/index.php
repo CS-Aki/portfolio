1e94499dd0f0d051b358f46be02d7f50219eeb79
CS-Aki <aquino.sir.victor.bscs2022@gmail.com>
2026-05-04 20:00:32 +0800

    update

--- public/index.php
+++ public/index.php
@@ -39,8 +39,7 @@
 
                     <div class="bg-[#0a1a2f] rounded-[10px] border border-[#1a644e] p-5 sm:p-6 w-full">
 
-                       <div class="flex flex-row flex-nowrap items-start justify-between gap-4 mb-4 w-full">
-                            <!-- Left side -->
+                       <div class="flex items-start w-full mb-4">
                             <div class="flex-1 min-w-0">
                                 <h3 class="text-[#40985e] text-lg sm:text-xl font-bold leading-snug mb-1">
                                     Full Stack Laravel Developer Intern
@@ -50,8 +49,7 @@
                                 </p>
                             </div>
 
-                            <!-- Right side -->
-                            <div class="flex-shrink-0 text-right">
+                            <div class="ml-auto flex-shrink-0 text-right">
                                 <span class="text-xs sm:text-sm text-[#d1cb95] opacity-60 whitespace-nowrap">
                                     April 2025 – July 2025
                                 </span>
