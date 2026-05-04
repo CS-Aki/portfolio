ebec5c3e1ffdd0159813b43efa0a0fe40a9d8510
CS-Aki <aquino.sir.victor.bscs2022@gmail.com>
2026-05-04 21:31:35 +0800

    change

--- public/index.php
+++ public/index.php
@@ -47,13 +47,14 @@
                 <!-- Profile image over circle -->
                 <div class="flex justify-center mt-8 lg:mt-0">
                     <div class="relative flex items-center justify-center w-[250px] h-[250px] sm:w-[350px] sm:h-[350px] lg:w-[400px] lg:h-[400px]">
-                        <svg class="absolute inset-0 w-full h-full" viewBox="0 0 100 100">
+                        <svg class="absolute inset-0 w-full h-full z-0" viewBox="0 0 100 100">
                             <circle cx="50" cy="50" r="45" fill="#1a644e" opacity="0.3"/>
                             <circle cx="50" cy="50" r="35" fill="#40985e" opacity="0.5"/>
                             <circle cx="50" cy="50" r="25" fill="#d1cb95" opacity="0.3"/>
                         </svg>
+
                         <img
-                            src=""
+                            src="img/profile.png"
                             alt="Victor Aquino"
                             class="relative z-10 w-[160px] h-[160px] sm:w-[220px] sm:h-[220px] lg:w-[260px] lg:h-[260px] rounded-full object-cover border-4 border-[#40985e] shadow-[0_0_30px_rgba(64,152,94,0.4)]"
                         />
